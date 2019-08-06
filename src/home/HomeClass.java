2828505a0a29d8488e57884bd74cc2ea11bef4c8
Marcel Schöni <marcel.schoeni@sbb.ch>
2019-08-06 07:01:55 +0200

    Update HomeClass.java

--- src/home/HomeClass.java
+++ src/home/HomeClass.java
@@ -3,7 +3,7 @@ package home;
 public class HomeClass {
 
 	public static void main(String[] args) {
-		System.out.println("I am a simple pipeline");
+		System.out.println("I am a simple pipeline with a change");
 	}
 
 }
